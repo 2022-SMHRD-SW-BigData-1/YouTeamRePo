416b0ef1a375829d419ef5658133e7c926bcbc89
smhrd <smhrd@DESKTOP-OUGQ933>
2022-09-01 19:55:12 +0900

    커밋 성공하기를,,

--- YouTeamPro/Controller/DAO.java
+++ YouTeamPro/Controller/DAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DAO {
 	Connection conn = null;
@@ -24,8 +25,100 @@ public class DAO {
 				System.out.println("접속 실패");
 			}
 		} catch (Exception e) {
+			System.out.println("오류");
 			e.printStackTrace();
 		}
 	}
+	private void close() {
+		try {
+			if (rs != null)
+				rs.close();
+			if (psmt != null)
+				psmt.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			System.out.println("자원반납 시 오류");
+		}
 
+	}
+	
+	// 회원가입 
+	public int join(String id, String pw, String nick) {
+		int cnt = 0;
+		try {
+			getCon();
+			String sql = "insert into user_Info values (?,?,?)";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+			psmt.setString(3, nick);
+
+			cnt = psmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	// 로그인
+	public boolean login(String id, String pw) {
+
+		try {
+			getCon();
+			
+			String sql = "select pw from user_Info where id = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			
+			rs = psmt.executeQuery();
+
+			if (rs.next()) {
+				if (rs.getString(1).contentEquals(pw)) {
+					return true; // 로그인 성공
+
+				}
+
+			}
+			return false; // 비밀번호 불일치 
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return false;
+	}
+	// 3.전체 랭킹확인 -> 수민팀장님 
+	
+	// 4.탈퇴 
+	public int delete(String id) {
+		int cnt = 0;
+		try {
+			getCon();
+			String sql = "delete from user_Info where id=?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+
+			cnt = psmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return cnt;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- YouTeamPro/View/Main.java
+++ YouTeamPro/View/Main.java
@@ -1,5 +1,9 @@
 package View;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import Controller.DAO;
@@ -10,24 +14,51 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		DAO dao = new DAO();
 		dao.getCon();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//기철
-		
 
+		while (true) {
+			System.out.println("[1]회원가입 [2]로그인 [3]전체랭킹확인 [4]회원탈퇴 [5]게임종료");
+			int menu = sc.nextInt();
+			if (menu == 1) {
+				System.out.println("==========등록==========");
+				System.out.print("ID : ");
+				String id = sc.next();
+				System.out.print("PW : ");
+				String pw = sc.next();
+				System.out.print("nick : ");
+				String nick = sc.next();
+
+				int cnt = dao.join(id, pw, nick);
+				if (cnt > 0) {
+					System.out.println("등록 성공");
+				} else {
+					System.out.println("등록 실패");
+				}
+			} else if (menu == 2) {
+
+				System.out.print("ID : ");
+				String id = sc.next();
+				System.out.print("PW : ");
+				String pw = sc.next();
+				boolean res = dao.login(id, pw);
+				if (res == true) {
+					System.out.println("로그인 성공");
+				} else {
+					System.out.println("로그인 실패");
+				}
+
+			} else if (menu == 3) {
+				// 수민팀장님
+			} else if (menu == 4) {
+				System.out.println("ID : ");
+				String id = sc.next();
+				System.out.println("PW : ");
+				int pw = sc.nextInt();
+				dao.delete(id);
+			} else if (menu == 5) {
+				System.out.println("게임 종료");
+				break;
+			}
+
+		}
 	}
-
 }
