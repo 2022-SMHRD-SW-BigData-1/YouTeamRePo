bfa6dd058abfc6c2c35fb7e0412fb31125692f77
smhrd <smhrd@DESKTOP-H9F7AQH>
2022-09-04 20:43:35 +0900

    dd

--- YouTeamPro/Controller/MusicPlayer.java
+++ YouTeamPro/Controller/MusicPlayer.java
@@ -6,14 +6,40 @@ import Model.MusicVO;
 import javazoom.jl.player.MP3Player;
 
 public class MusicPlayer {
+
 	MP3Player mp3 = new MP3Player();
 	ArrayList<MusicVO> musiclist = new ArrayList<MusicVO>();
-	int currentIndex=0;
-	
+
 	public MusicPlayer() {
-		
+		musiclist.add(new MusicVO("0_프롤로그", "C://music/0_프롤로그.mp3"));
+		musiclist.add(new MusicVO("1_스토리진행", "C://music/1_스토리진행.mp3"));
+		musiclist.add(new MusicVO("2_스핑크스", "C://music/2_스핑크스.mp3"));
+		musiclist.add(new MusicVO("3_회원정보", "C://music/3_회원정보.mp3"));
+		musiclist.add(new MusicVO("test청하", "C://music/test청하.mp3"));
+		musiclist.add(new MusicVO("5_개인정보", "C://music/5_개인정보.mp3"));
+		musiclist.add(new MusicVO("0_프롤로그", "C://music/0_프롤로그"));
+		musiclist.add(new MusicVO("0_프롤로그", "C://music/0_프롤로그"));
+		musiclist.add(new MusicVO("0_프롤로그", "C://music/0_프롤로그"));
+	
 	}
-	
-	
-	
+
+	public MusicVO play(int a) {
+		MusicVO m = musiclist.get(a);
+
+		if (mp3.isPlaying()) {
+			mp3.stop();
+		}
+
+		mp3.play(m.getMusicPath());
+
+		return m;
+
+	}
+
+	public void stop() {
+		if (mp3.isPlaying()) {
+			mp3.stop();
+		}
+	}
+
 }
--- YouTeamPro/Model/Story.java
+++ YouTeamPro/Model/Story.java
@@ -1,11 +1,16 @@
 package Model;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import Controller.MusicPlayer;
+import javazoom.jl.player.MP3Player;
 
 public class Story {
 	// 글자 멈춤 메소드
-
+	MP3Player mp3 = new MP3Player();
+	ArrayList<MusicVO> musiclist = new ArrayList<MusicVO>();
 	private static void pause() {
 		try {
 			System.in.read();
@@ -28,6 +33,8 @@ public class Story {
 	// 메인 타이틀
 
 	public static void title() {
+		MusicPlayer player = new MusicPlayer();
+		MusicVO m = player.play(0);
 		slowPrint(
 				"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@###@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
 						+ "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*#;@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
@@ -106,8 +113,11 @@ public class Story {
 						+ "@@@@@@@@@@@*$@@@@#@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
 						+ "@@@@@@@@@@@@;@~-@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
 						+ "@@@@@@@@@@@@=#@@@@@@$@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
-				10); // 옆에 숫자는 출력되는 글자들 빠르기 조절
+				1
+				
+				); // 옆에 숫자는 출력되는 글자들 빠르기 조절
 		System.out.println();
+		player.stop();
 	}
 	// 오프닝 멘트
 
@@ -116,6 +126,9 @@ public class Story {
 		System.out.print("Enter 키를 누르시면 스토리가 진행됩니다.");
 		pause();
 		pause();
+		MusicPlayer player = new MusicPlayer();
+		MusicVO m = player.play(1);
+		
 		String[] openingText1 = { "잊혀진 고대왕국의 주인인 사막황제의 무덤이 발견되었다는 소식이 왕국전체에 퍼졌다", "\t",
 				"왕국최고의 트레져헌터팀인 우리는 소식이 퍼지기 전에 미리 무덤입구 앞에 도달했다", "\t", "입구에는 고대속 괴수인 스핑크스가 그려져 있었다",
 				"\t" + "겨우겨우 숨겨진 입구를 찾아왔건만 스핑크스가 지키는 무덤일 줄은 상상도 못했다", "\t",
@@ -123,10 +136,17 @@ public class Story {
 		for (int i = 0; i < openingText1.length; i++) {
 			slowPrint(openingText1[i] + "\n", 20);
 		}
+		player.stop();
 		String[] openingText2 = { "우리는 무덤안에 들어가지 말았어야 했.. 다..." };
 		for (int i = 0; i < openingText2.length; i++) {
 			slowPrint(openingText2[i] + "\n", 50);
 		}
+		System.out.print("Enter 키를 누르면 피라미드로 입장합니다.");
+		pause();
+		pause();
+
+
+		m = player.play(2);
 		String[] openingText3 = { "생각보다 별거 없는데 얼른 보물방 찾아서 도굴하고 돌아가ㅈㅏ...", "\t", "콰아아아!!! 이즈리얼 살려줘!!! 누가 다리를 붙잡고있어!",
 				"\t", "돌아가라고 친절하게 그림까지 그려 놨건만 내가 없는줄 알고 들어왔지?", "\t" + "내가 내는 문제를 맞추지 못하면 니 친구는 내 뱃속으로 다이빙이다", "\t",
 				"나 스핑크스가 내는 문제를 맞춰서 친구를 살려봐라!!!!" };
@@ -138,20 +158,23 @@ public class Story {
 		for (int i = 0; i < openingText4.length; i++) {
 			slowPrint(openingText4[i] + "\n", 20);
 		}
-		String[] openingText5 = { "어자피 도둑놈의 인생 이렇게 될 줄 알고있었다 버리고 도망갈까? (Y/N)" };
+		String[] openingText5 = { "어자피 도둑놈의 인생 이렇게 될 줄 알고있었다 버리고 도망갈까? (y/n)" };
 		for (int i = 0; i < openingText5.length; i++) {
 			slowPrint(openingText5[i] + "\n", 20);
 		}
 		while (true) {
 			System.out.print("입력해주세요 : ");
 			String click = sc.next();
-			if (click.equals("Y")) {
+			if (click.equals("y")) {
+
 				System.out.println();
-				slowPrint("의리도 없는 놈 같으니라고 너부터 먹어야 겠다", 10);
+				slowPrint("의리도 없는 놈 같으니라고 넌 강제 시작이다.", 10);
 				System.out.println();
 				System.out.println();
 				break;
-			} else if (click.equals("N")) {
+			} else if (click.equals("n")) {
+				player.stop();
+				
 				slowPrint("맞춰봐라! 틀릴때마다 니 친구의 팔 다리는 내가 맛보겠다!", 10);
 				System.out.println();
 				System.out.println();
@@ -160,6 +183,8 @@ public class Story {
 				slowPrint("잘못 입력하셨습니다.", 10);
 				System.out.println();
 			}
+		
+			
 		}
 	}
 }
